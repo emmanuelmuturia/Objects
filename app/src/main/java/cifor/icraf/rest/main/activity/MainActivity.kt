2d8cce4a0e2064cb4cd83eec1718d8c49053a42e
Emmanuel Muturia™ <science@emmanuelmuturia.com>
2024-07-16 13:11:24 +0300

    Set up the package structure...

--- app/src/main/java/cifor/icraf/rest/main/activity/MainActivity.kt
+++ app/src/main/java/cifor/icraf/rest/main/activity/MainActivity.kt
@@ -1,10 +1,11 @@
-package cifor.icraf.rest
+package cifor.icraf.rest.main.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import cifor.icraf.rest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
