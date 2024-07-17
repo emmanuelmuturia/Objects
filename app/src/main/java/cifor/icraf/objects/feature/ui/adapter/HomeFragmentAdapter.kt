e06d62029a05e99bbd3d8711f9cca8b900ae08bb
Emmanuel Muturia™ <science@emmanuelmuturia.com>
2024-07-17 12:21:13 +0300

    Set up the Home Fragment Adapter and extract the String Resources...

---
+++ app/src/main/java/cifor/icraf/objects/feature/ui/adapter/HomeFragmentAdapter.kt
@@ -0,0 +1,53 @@
+package cifor.icraf.objects.feature.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import cifor.icraf.objects.R
+import cifor.icraf.objects.feature.data.model.Object
+import cifor.icraf.objects.feature.ui.diffutil.ObjectDiffItemCallback
+
+class HomeFragmentAdapter(
+    private val onCardClicked: (String) -> Unit,
+    private val onCardButtonClicked: () -> Unit
+) : ListAdapter<Object, HomeFragmentAdapter.ObjectItemViewHolder>(ObjectDiffItemCallback()) {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ObjectItemViewHolder = ObjectItemViewHolder.inflateFrom(parent)
+
+    override fun onBindViewHolder(holder: ObjectItemViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item = item)
+    }
+
+    class ObjectItemViewHolder(rootView: CardView) : RecyclerView.ViewHolder(rootView) {
+
+        private val objectIdValue = rootView.findViewById<TextView>(R.id.objectIdValue)
+
+        private val objectNameValue = rootView.findViewById<TextView>(R.id.objectNameValue)
+
+        companion object {
+            fun inflateFrom(viewGroup: ViewGroup): ObjectItemViewHolder {
+                val layoutInflater = LayoutInflater.from(viewGroup.context)
+                val view = layoutInflater.inflate(
+                    R.layout.object_item,
+                    viewGroup,
+                    false
+                ) as CardView
+                return ObjectItemViewHolder(view)
+            }
+        }
+
+        fun bind(item: Object) {
+            objectIdValue.text = item.objectId
+            objectNameValue.text = item.objectName
+        }
+
+    }
+
+}
