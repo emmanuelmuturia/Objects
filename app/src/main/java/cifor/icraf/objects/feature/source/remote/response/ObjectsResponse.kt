10c4ec3cbd2a03964498f164398c829b17121c41
Emmanuel Muturia™ <science@emmanuelmuturia.com>
2024-07-17 14:57:44 +0300

    Set up the POST Request...

--- app/src/main/java/cifor/icraf/objects/feature/source/remote/response/ObjectsResponse.kt
+++ app/src/main/java/cifor/icraf/objects/feature/source/remote/response/ObjectsResponse.kt
@@ -1,11 +1,13 @@
-package cifor.icraf.objects.feature.source.remote.dto
+package cifor.icraf.objects.feature.source.remote.response
 
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ObjectsDTO(
+data class ObjectsResponse(
+    @SerialName("createdAt")
+    val objectCreatedAt: String,
     @SerialName("id")
     val objectId: String,
     @SerialName("name")
--- app/src/main/java/cifor/icraf/objects/feature/source/remote/service/ObjectsApi.kt
+++ app/src/main/java/cifor/icraf/objects/feature/source/remote/service/ObjectsApi.kt
@@ -1,12 +1,18 @@
 package cifor.icraf.objects.feature.source.remote.service
 
 import cifor.icraf.objects.feature.source.remote.dto.ObjectsDTO
+import cifor.icraf.objects.feature.source.remote.response.ObjectsResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
-fun interface ObjectsApi {
+interface ObjectsApi {
 
     @GET(value = "objects")
     suspend fun getAllObjects() : Response<List<ObjectsDTO>>
+
+    @POST(value = "objects")
+    suspend fun putObject(@Body objectsDTO: ObjectsDTO): Response<ObjectsResponse>
 
 }
