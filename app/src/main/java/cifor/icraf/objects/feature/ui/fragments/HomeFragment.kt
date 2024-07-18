172729c03e128aea6afdf57505608381f6eb724a
Emmanuel Muturia™ <science@emmanuelmuturia.com>
2024-07-18 09:22:46 +0300

    Set up Logging to confirm the presence of requested data...

--- app/src/main/java/cifor/icraf/objects/feature/ui/fragments/HomeFragment.kt
+++ app/src/main/java/cifor/icraf/objects/feature/ui/fragments/HomeFragment.kt
@@ -1,6 +1,7 @@
 package cifor.icraf.objects.feature.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +14,7 @@ import cifor.icraf.objects.feature.ui.viewmodel.ObjectsViewModel
 import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import timber.log.Timber
 
 class HomeFragment : Fragment() {
 
