c2d31b86e1d34e133bcf946f62f019d401279e49
Emmanuel Muturia™ <science@emmanuelmuturia.com>
2024-08-14 15:41:22 +0300

    Upgrade libraries and clean up onDestroyView()...

--- Objects/app/src/main/java/cifor/icraf/objects/feature/ui/fragments/HomeFragment.kt
+++ Objects/app/src/main/java/cifor/icraf/objects/feature/ui/fragments/HomeFragment.kt
@@ -1,20 +1,15 @@
 package cifor.icraf.objects.feature.ui.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import cifor.icraf.objects.databinding.FragmentHomeBinding
 import cifor.icraf.objects.feature.ui.adapter.HomeFragmentAdapter
 import cifor.icraf.objects.feature.ui.viewmodel.ObjectsViewModel
-import kotlinx.coroutines.cancelChildren
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
@@ -75,7 +70,5 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-        //binding.objectList.adapter = null
-        //viewLifecycleOwner.lifecycleScope.coroutineContext.cancelChildren()
     }
 }
